ab7c98ee268b3cc0a7e5fe0d466620c191f58d40
MarlonPelau <MarlonRegis@pursuit.org>
2024-07-05 11:32:28 -0400

    some style and format changes in about css and jsx files

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -26,6 +26,11 @@ const About = () => {
          </p>
 
           <h2>Armando</h2>
+          <p className="linkedin-link">
+         <a href="https://www.linkedin.com/in/armando-pires-aba0a121b/" target="_blank" rel="noopener noreferrer">
+         Connect with him on LinkedIn
+         </a>
+         </p>
           <a
             href="https://github.com/ArmandoPires103"
             className="github-link"
@@ -38,12 +43,13 @@ const About = () => {
               height="300"
             />
           </a>
+          
+          <h2>Brenda</h2>
           <p className="linkedin-link">
-         <a href="https://www.linkedin.com/in/armando-pires-aba0a121b/" target="_blank" rel="noopener noreferrer">
-         Connect with him on LinkedIn
+         <a href="https://www.linkedin.com/in/brendasotoct/" target="_blank" rel="noopener noreferrer">
+         Connect with her on LinkedIn
          </a>
          </p>
-          <h2>Brenda</h2>
           <a
             href="https://github.com/BSoto85"
             className="github-link"
@@ -56,12 +62,13 @@ const About = () => {
               height="300"
             />
           </a>
+          
+          <h2>Luis</h2>
           <p className="linkedin-link">
-         <a href="https://www.linkedin.com/in/brendasotoct/" target="_blank" rel="noopener noreferrer">
-         Connect with her on LinkedIn
+         <a href="https://www.linkedin.com/in/luis-tejada-56538b271/" target="_blank" rel="noopener noreferrer">
+         Connect with him on LinkedIn
          </a>
          </p>
-          <h2>Luis</h2>
           <a
             href="https://github.com/Blui322"
             className="github-link"
@@ -74,12 +81,13 @@ const About = () => {
               height="300"
             />
           </a>
+          
+          <h2>Marlon</h2>
           <p className="linkedin-link">
-         <a href="https://www.linkedin.com/in/luis-tejada-56538b271/" target="_blank" rel="noopener noreferrer">
+         <a href="https://www.linkedin.com/in/mrmarlon/" target="_blank" rel="noopener noreferrer">
          Connect with him on LinkedIn
          </a>
          </p>
-          <h2>Marlon</h2>
           <a
             href="https://github.com/MarlonPelau"
             className="github-link"
@@ -92,11 +100,6 @@ const About = () => {
               height="300"
             />
           </a>
-          <p className="linkedin-link">
-         <a href="https://www.linkedin.com/in/mrmarlon/" target="_blank" rel="noopener noreferrer">
-         Connect with him on LinkedIn
-         </a>
-         </p>
         </li>
       </ul>
     </div>
