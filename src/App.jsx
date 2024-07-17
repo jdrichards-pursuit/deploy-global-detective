8ab19212b008ec289287fd29b8a35ddc805233aa
Blui322 <luistejada@pursuit.com>
2024-07-17 10:11:06 -0400

    leaderboard done

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
-
-import { useEffect, useState } from 'react'
-import { Routes, Route, Navigate } from 'react-router-dom'
-import { ToastContainer } from 'react-toastify'
-import { getUserData } from './helpers/getUserData.js'
+import { useEffect, useState } from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+//import { getUserData } from './helpers/getUserData.js'
 
 import { auth } from "./helpers/firebase";
 
@@ -10,81 +9,98 @@ import Login from "./Components/Login";
 import SignUpView from "./Pages/SignUpView.jsx";
 // import Profile from './Components/Profile'
 
-import Test from './Components/Test'
-import HomePage from './Pages/HomePage'
-import CountriesPage from './Pages/CountriesPage.jsx'
-import LeaderboardPage from './Pages/LeaderboardPage.jsx'
-import CaseFilesPage from './Pages/CaseFilesPage.jsx'
-import CaseDetailsPage from './Pages/CaseDetailsPage.jsx'
-import CasePhotosPage from './Pages/CasePhotosPage.jsx'
-import QuestionsPage from './Pages/QuestionsPage.jsx'
-import ResultsPage from './Pages/ResultPage.jsx'
+import Test from "./Components/Test";
+import HomePage from "./Pages/HomePage";
+import CountriesPage from "./Pages/CountriesPage.jsx";
+import LeaderboardPage from "./Pages/LeaderboardPage.jsx";
+import CaseFilesPage from "./Pages/CaseFilesPage.jsx";
+import CaseDetailsPage from "./Pages/CaseDetailsPage.jsx";
+import CasePhotosPage from "./Pages/CasePhotosPage.jsx";
+import QuestionsPage from "./Pages/QuestionsPage.jsx";
+import ResultsPage from "./Pages/ResultPage.jsx";
 
-import 'react-toastify/dist/ReactToastify.css'
-import './App.css'
-import ProfilePage from './Pages/ProfilePage.jsx'
-
+import "react-toastify/dist/ReactToastify.css";
+import "./App.css";
+import ProfilePage from "./Pages/ProfilePage.jsx";
 
 function App() {
-  const [user, setUser] = useState();
-  useEffect(() => {
-    auth.onAuthStateChanged((user) => {
-      setUser(user);
-    });
-  });
-  // PROP FOR COUNTRY FETCH
-  const [countries, setCountries] = useState([]);
+	const [user, setUser] = useState();
+	useEffect(() => {
+		auth.onAuthStateChanged((user) => {
+			setUser(user);
+		});
+	});
+	// PROP FOR COUNTRY FETCH
+	const [countries, setCountries] = useState([]);
 
-  useEffect(() => {
-    const fetchCountries = async () => {
-      try {
-        const response = await fetch("http://localhost:3003/api/countries");
-        const data = await response.json();
-        setCountries(data);
-      } catch (error) {
-        console.error("Error fetching countries:", error);
-      }
-    };
+	useEffect(() => {
+		const fetchCountries = async () => {
+			try {
+				const response = await fetch("http://localhost:3003/api/countries");
+				const data = await response.json();
+				setCountries(data);
+			} catch (error) {
+				console.error("Error fetching countries:", error);
+			}
+		};
 
-    fetchCountries();
-  }, []);
+		fetchCountries();
+	}, []);
 
-  return (
-    <div>
-      <Routes
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "center",
-          alignItems: "center",
-          marginTop: 100,
-        }}
-      >
-        {/* <Route
+	return (
+		<div>
+			<Routes
+				style={{
+					display: "flex",
+					flexDirection: "column",
+					justifyContent: "center",
+					alignItems: "center",
+					marginTop: 100,
+				}}
+			>
+				{/* <Route
           path="/"
           element={user ? <Navigate to="/profile" /> : <Login />}
         /> */}
-        <Route path="/" element={<HomePage />} />
-        <Route path="/test" element={user ? <Test /> : <Login />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<SignUpView />} />
-        <Route path="/profile" element={<ProfilePage />} />
-        {/* <Route path="/help" element={<HelpPage />} />
+				<Route path="/" element={<HomePage />} />
+				<Route path="/test" element={user ? <Test /> : <Login />} />
+				<Route path="/login" element={<Login />} />
+				<Route path="/register" element={<SignUpView />} />
+				<Route path="/profile" element={<ProfilePage />} />
+				{/* <Route path="/help" element={<HelpPage />} />
         <Route path="/about" element={<AboutPage />} /> */}
-        {/* <Route path="/leaderboard" element={<LeaderboardPage />} /> */}
-        {/* <Route path="/achievements" element={<AchievementsPage />} /> */}
-        <Route path="/countries" element={<CountriesPage countries={countries}/>} />
-        <Route path="/countries/:countryId/casefiles" element={<CaseFilesPage countries={countries}/>} />
-        <Route exact path="/countries/:countryId/case_files/:caseFileId" element={<CaseDetailsPage />} />
-        <Route path="/countries/:countriesId/case_files/:caseFileId/photos" element={<CasePhotosPage />} />
-        <Route path="/countries/:countryId/case_files/:caseFileId/questions" element={<QuestionsPage />} />
-        <Route path="/countries/:countryId/case_files/:caseFileId/questions/results/:score/:totalQuestions" element={<ResultsPage />} />
-        {/* <Route path="*" element={<FourOFourPage />} /> */}
-
-      </Routes>
-      <ToastContainer />
-    </div>
-  );
+				<Route path="/leaderboard" element={<LeaderboardPage />} />
+				{/* <Route path="/achievements" element={<AchievementsPage />} /> */}
+				<Route
+					path="/countries"
+					element={<CountriesPage countries={countries} />}
+				/>
+				<Route
+					path="/countries/:countryId/casefiles"
+					element={<CaseFilesPage countries={countries} />}
+				/>
+				<Route
+					exact
+					path="/countries/:countryId/case_files/:caseFileId"
+					element={<CaseDetailsPage />}
+				/>
+				<Route
+					path="/countries/:countriesId/case_files/:caseFileId/photos"
+					element={<CasePhotosPage />}
+				/>
+				<Route
+					path="/countries/:countryId/case_files/:caseFileId/questions"
+					element={<QuestionsPage />}
+				/>
+				<Route
+					path="/countries/:countryId/case_files/:caseFileId/questions/results/:score/:totalQuestions"
+					element={<ResultsPage />}
+				/>
+				{/* <Route path="*" element={<FourOFourPage />} /> */}
+			</Routes>
+			<ToastContainer />
+		</div>
+	);
 }
 
 export default App;
--- src/Pages/LeaderboardPage.jsx
+++ src/Pages/LeaderboardPage.jsx
@@ -1,12 +1,115 @@
-import React from 'react'
-import Navbar from '../Components/NavBar'
-
+import React, { useEffect, useState } from "react";
+import Navbar from "../Components/NavBar";
+import "../CSS/LeaderboardPage.css";
+import { useNavigate } from "react-router-dom";
 const LeaderboardPage = () => {
-  return (
-    <div>LeaderboardPage
-        <Navbar/>
-    </div>
-  )
-}
+	const [usersXP, setUsersXP] = useState([]);
+	const [xpToBeatNext, setXpToBeatNext] = useState(0);
+	const [nextUser, setNextUser] = useState("");
+	const currentUser = "";
+	const navigate = useNavigate;
 
-export default LeaderboardPage
+	// useEffect(() => {
+	// 	const data = [
+	// 		{ id: 1, username: "user1", points: 1500 },
+	// 		{ id: 2, username: "user2", points: 1000 },
+	// 		{ id: 3, username: "user3", points: 600 },
+	// 		{ id: 4, username: "user4", points: 200 },
+	// 		{ id: 5, username: "user5", points: 150 },
+	// 		{ id: 6, username: "user6", points: 125 },
+	// 		{ id: 7, username: "user7", points: 75 },
+	// 		{ id: 8, username: "user8", points: 50 },
+	// 		{ id: 1, username: "user1", points: 1500 },
+	// 		{ id: 2, username: "user2", points: 1000 },
+	// 		{ id: 3, username: "user3", points: 600 },
+	// 		{ id: 4, username: "user4", points: 200 },
+	// 		{ id: 5, username: "user5", points: 150 },
+	// 		{ id: 6, username: "user6", points: 125 },
+	// 		{ id: 7, username: "user7", points: 75 },
+	// 		{ id: 8, username: "user8", points: 50 },
+	// 	];
+	// 	setUsersXP(data);
+
+	// 	const currentUserIndex = data.findIndex(
+	// 		(user) => user.username === currentUser
+	// 	);
+	// 	if (currentUserIndex > 0) {
+	// 		setXpToBeatNext(
+	// 			data[currentUserIndex - 1].points - data[currentUserIndex].points
+	// 		);
+	// 		setNextUser(data[currentUserIndex - 1].username);
+	// 	}
+	// }, [currentUser]);
+
+	useEffect(() => {
+		const fetchUsersXP = async () => {
+			try {
+				const response = await fetch(
+					"http://localhost:3003/api/stats/leaderboard"
+				);
+				if (!response.ok) {
+					throw new Error("Network response was not ok");
+				}
+				const data = await response.json();
+				setUsersXP(data);
+				const currentUserIndex = data.findIndex(
+					(user) => user.username === currentUser
+				);
+				if (currentUserIndex > 0) {
+					setXpToBeatNext(
+						data[currentUserIndex - 1].points - data[currentUserIndex].points
+					);
+					setNextUser(data[currentUserIndex - 1].username);
+				}
+			} catch (error) {
+				console.error("Error fetching users and XP:", error);
+			}
+		};
+		fetchUsersXP();
+	}, []);
+
+	return (
+		<div className="leaderboard-container">
+			<Navbar />
+			<h1>LEADERBOARD</h1>
+			<div className="leaderboard">
+				<table>
+					<thead>
+						<tr className="underlinenames">
+							<th>Rank</th>
+							<th>Name</th>
+							<th>XP</th>
+						</tr>
+					</thead>
+					<tbody>
+						{usersXP.map((user, index) => (
+							<tr
+								key={user.id}
+								className={user.username === currentUser ? "current-user" : ""}
+							>
+								<td className="rankholder">{index + 1}</td>
+								<td className="nameholder">
+									{user.first_name} {user.last_name[0]}
+								</td>
+								<td className="xpholder">{user.xp}</td>
+							</tr>
+						))}
+					</tbody>
+				</table>
+			</div>
+			<div className="investigation-info">
+				<p>
+					You're only {xpToBeatNext} XP away from beating @{nextUser}!
+				</p>
+				<button
+					className="buttonest"
+					onClick={() => console.log("Open an Investigation!")}
+				>
+					OPEN AN INVESTIGATION
+				</button>
+			</div>
+		</div>
+	);
+};
+
+export default LeaderboardPage;
