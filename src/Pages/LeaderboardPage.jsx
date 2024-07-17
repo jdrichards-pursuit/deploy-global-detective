630812230338b91ef0768102f71302b18263013d
Blui322 <luistejada@pursuit.com>
2024-07-17 16:02:18 -0400

    learderboard done

--- src/Pages/LeaderboardPage.jsx
+++ src/Pages/LeaderboardPage.jsx
@@ -2,44 +2,13 @@ import React, { useEffect, useState } from "react";
 import Navbar from "../Components/NavBar";
 import "../CSS/LeaderboardPage.css";
 import { useNavigate } from "react-router-dom";
+
 const LeaderboardPage = () => {
 	const [usersXP, setUsersXP] = useState([]);
 	const [xpToBeatNext, setXpToBeatNext] = useState(0);
 	const [nextUser, setNextUser] = useState("");
 	const currentUser = "";
-	const navigate = useNavigate;
-
-	// useEffect(() => {
-	// 	const data = [
-	// 		{ id: 1, username: "user1", points: 1500 },
-	// 		{ id: 2, username: "user2", points: 1000 },
-	// 		{ id: 3, username: "user3", points: 600 },
-	// 		{ id: 4, username: "user4", points: 200 },
-	// 		{ id: 5, username: "user5", points: 150 },
-	// 		{ id: 6, username: "user6", points: 125 },
-	// 		{ id: 7, username: "user7", points: 75 },
-	// 		{ id: 8, username: "user8", points: 50 },
-	// 		{ id: 1, username: "user1", points: 1500 },
-	// 		{ id: 2, username: "user2", points: 1000 },
-	// 		{ id: 3, username: "user3", points: 600 },
-	// 		{ id: 4, username: "user4", points: 200 },
-	// 		{ id: 5, username: "user5", points: 150 },
-	// 		{ id: 6, username: "user6", points: 125 },
-	// 		{ id: 7, username: "user7", points: 75 },
-	// 		{ id: 8, username: "user8", points: 50 },
-	// 	];
-	// 	setUsersXP(data);
-
-	// 	const currentUserIndex = data.findIndex(
-	// 		(user) => user.username === currentUser
-	// 	);
-	// 	if (currentUserIndex > 0) {
-	// 		setXpToBeatNext(
-	// 			data[currentUserIndex - 1].points - data[currentUserIndex].points
-	// 		);
-	// 		setNextUser(data[currentUserIndex - 1].username);
-	// 	}
-	// }, [currentUser]);
+	const navigate = useNavigate();
 
 	useEffect(() => {
 		const fetchUsersXP = async () => {
@@ -52,6 +21,7 @@ const LeaderboardPage = () => {
 				}
 				const data = await response.json();
 				setUsersXP(data);
+
 				const currentUserIndex = data.findIndex(
 					(user) => user.username === currentUser
 				);
@@ -66,7 +36,7 @@ const LeaderboardPage = () => {
 			}
 		};
 		fetchUsersXP();
-	}, []);
+	}, [currentUser]);
 
 	return (
 		<div className="leaderboard-container">
